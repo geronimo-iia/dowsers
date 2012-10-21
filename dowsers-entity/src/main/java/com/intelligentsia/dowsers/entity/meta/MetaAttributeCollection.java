c485d70f5d87bf0b0afd9adabae2f738cef5f0e5
Jerome Guibert <jguibert@intelligents-ia.com>
2012-10-21 21:04:14 +0200

    finalize attributes collections

--- dowsers-entity/src/main/java/com/intelligentsia/dowsers/entity/meta/MetaAttributeCollection.java
+++ dowsers-entity/src/main/java/com/intelligentsia/dowsers/entity/meta/MetaAttributeCollection.java
@@ -3,25 +3,47 @@ package com.intelligentsia.dowsers.entity.meta;
 import java.io.Serializable;
 import java.util.Iterator;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
+/**
+ * MetaAttributeCollection is a {@link ImmutableCollection} of
+ * {@link MetaAttribute}.
+ * 
+ * @author <a href="mailto:jguibert@intelligents-ia.com" >Jerome Guibert</a>
+ */
 public class MetaAttributeCollection implements Iterable<MetaAttribute>, Serializable {
 
 	/**
 	 * serialVersionUID:long
 	 */
 	private static final long serialVersionUID = -4410480905712842651L;
-	
-	private ImmutableCollection<MetaAttribute> metaAttributes;
 
-	public MetaAttributeCollection() {
+	/**
+	 * {@link ImmutableCollection} {@link MetaAttribute}.
+	 */
+	private final ImmutableCollection<MetaAttribute> metaAttributes;
+
+	/**
+	 * Build a new instance of <code>MetaAttributeCollection</code>.
+	 * For serialization purpose.
+	 */
+	@SuppressWarnings("unused")
+	private MetaAttributeCollection() {
 		super();
+		metaAttributes = ImmutableSet.of();
 	}
 
+	/**
+	 * Build a new instance of <code>MetaAttributeCollection</code>.
+	 * 
+	 * @param metaAttributes
+	 */
 	public MetaAttributeCollection(ImmutableCollection<MetaAttribute> metaAttributes) {
 		super();
-		this.metaAttributes = metaAttributes;
+		this.metaAttributes = Preconditions.checkNotNull(metaAttributes);
 	}
 
 	@Override
@@ -33,16 +55,20 @@ public class MetaAttributeCollection implements Iterable<MetaAttribute>, Seriali
 		return metaAttributes;
 	}
 
-	public void setMetaAttributes(ImmutableCollection<MetaAttribute> metaAttributes) {
-		this.metaAttributes = metaAttributes;
-	}
-
 	public boolean contains(Object object) {
 		return metaAttributes.contains(object);
 	}
 
 	public ImmutableList<MetaAttribute> asList() {
 		return metaAttributes.asList();
+	}
+
+	public boolean isEmpty() {
+		return metaAttributes.isEmpty();
+	}
+
+	public int size() {
+		return metaAttributes.size();
 	}
 
 }
