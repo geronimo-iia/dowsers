83b3de2d2f7755b47b9a1c22205b1fae5285162d
Jerome Guibert <jguibert@intelligents-ia.com>
2012-11-08 17:07:18 +0100

    first impl of Meta entity provider analyzer

--- dowsers-entity-meta-provider/src/main/java/com/intelligentsia/dowsers/entity/meta/provider/MetaEntityProviderAnalyzer.java
+++ dowsers-entity-meta-provider/src/main/java/com/intelligentsia/dowsers/entity/meta/provider/MetaEntityProviderAnalyzer.java
@@ -22,15 +22,27 @@
  */
 package com.intelligentsia.dowsers.entity.meta.provider;
 
+import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.intelligentsia.dowsers.core.reflection.ClassInformation;
+import org.intelligentsia.dowsers.core.reflection.Reflection;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.intelligentsia.dowsers.entity.Entity;
+import com.intelligentsia.dowsers.entity.annotation.Attribute;
+import com.intelligentsia.dowsers.entity.annotation.DomainEntity;
+import com.intelligentsia.dowsers.entity.annotation.IgnoreAttribute;
+import com.intelligentsia.dowsers.entity.meta.MetaAttribute;
 import com.intelligentsia.dowsers.entity.meta.MetaEntity;
 import com.intelligentsia.dowsers.entity.meta.MetaEntityProvider;
+import com.intelligentsia.dowsers.entity.meta.MetaModel;
 import com.intelligentsia.dowsers.entity.reference.Reference;
 
 /**
@@ -57,18 +69,116 @@ public class MetaEntityProviderAnalyzer implements MetaEntityProvider {
 		// analyze
 		final MetaEntity metaEntity = analyze(classInformation);
 		if (metaEntity != null) {
-			result.add(metaEntity);
+			if (!metaEntity.metaAttributes().isEmpty()) {
+				result.add(metaEntity);
+			}
 		}
 		return result;
 	}
 
+	/**
+	 * @param classInformation
+	 *            class Information to analyze
+	 * @return a {@link MetaEntity} instance
+	 * @throws NullPointerException
+	 *             if classInformation is null
+	 */
+	@VisibleForTesting
 	public MetaEntity analyze(final ClassInformation classInformation) throws NullPointerException {
-		// TODO implements it
-		
-		
-		
-		
-		return null;
+		Preconditions.checkNotNull(classInformation);
+		final Class<?> clazz = classInformation.getType();
+		// init builder with class name and MetaModel version.
+		final MetaEntity.Builder builder = MetaEntity.builder().name(clazz.getName()).version(MetaModel.VERSION);
+		// analyze attributes
+		final Iterator<MetaAttribute> iterator = analyzeMetaAttribute(clazz).iterator();
+		while (iterator.hasNext()) {
+			builder.metaAttribute(iterator.next());
+		}
+		return builder.build();
 	}
 
+	/**
+	 * Analyze MetaAttribute on specified class.
+	 * 
+	 * @param clazz
+	 * @return an instance of {@link List} of {@link MetaAttribute}
+	 * @throws NullPointerException
+	 *             if clazz is null
+	 * @throws IllegalArgumentException
+	 *             if something was wrong with attribute declaration
+	 */
+	@VisibleForTesting
+	public Collection<MetaAttribute> analyzeMetaAttribute(final Class<?> clazz) throws NullPointerException, IllegalArgumentException {
+		final Map<String, MetaAttribute> attributes = Maps.newHashMap();
+		Preconditions.checkNotNull(clazz);
+
+		// auto discover attributes
+		final DomainEntity domainEntity = clazz.getAnnotation(DomainEntity.class);
+		final boolean autoDiscovering = domainEntity != null ? domainEntity.autoDiscovering() : Boolean.TRUE;
+
+		// for each method
+		for (final Method method : clazz.getMethods()) {
+			if (hasAttributeSignature(method, autoDiscovering)) {
+				final String name = extractName(method);
+				if (!attributes.containsKey(name)) {
+					attributes.put(name, MetaAttribute.builder().name(name).valueClass(extractValueClass(method)).build());
+				}
+			}
+		}
+
+		// check for Identity attribute
+		if (!attributes.containsKey(MetaModel.getIdentityAttribute().name())) {
+			attributes.put(MetaModel.getIdentityAttribute().name(), MetaModel.getIdentityAttribute());
+		} else {
+			// check Identity attribute compliance
+			if (!attributes.get(MetaModel.getIdentityAttribute().name()).equals(MetaModel.getIdentityAttribute())) {
+				throw new IllegalArgumentException("Declared 'identity' attribute must be a Reference type");
+			}
+		}
+		return attributes.values();
+	}
+
+	/**
+	 * Extract a method name.
+	 * 
+	 * @param method
+	 * @return a name
+	 */
+	@VisibleForTesting
+	public String extractName(final Method method) {
+		final String name = method.getName();
+		if (name.startsWith("get") || name.startsWith("set")) {
+			return Reflection.toFieldName(name);
+		}
+		return Reflection.uncapitalize(name);
+	}
+
+	@VisibleForTesting
+	public Class<?> extractValueClass(final Method method) {
+		if (Void.TYPE == method.getReturnType()) {
+			return method.getParameterTypes()[0];
+		}
+		return method.getReturnType();
+	}
+
+	/**
+	 * Check if the specified method can be an attribute.
+	 * 
+	 * @param method
+	 * @return true if specified method can be an attribute, false other else.
+	 */
+	private boolean hasAttributeSignature(final Method method, final boolean autoDiscovering) {
+		if (method.getAnnotation(IgnoreAttribute.class) == null) {
+			return false;
+		}
+		if (!autoDiscovering && (method.getAnnotation(Attribute.class) == null)) {
+			return false;
+		}
+		if (Void.TYPE == method.getReturnType()) {
+			return method.getParameterTypes().length == 1;
+		} else {
+			return method.getParameterTypes().length == 0;
+		}
+
+	}
 }
